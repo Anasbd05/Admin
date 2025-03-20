e0930bb87e222643972bebfd77bf336fbc6491ed
anas bd <anastrying05@gmail.com>
2025-03-20 12:24:51 +0000

    fix issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import ProductPage from "@/app/products/page";
 import Sidebar from "@/constant/Sidebar";
+import ProductPage from "./products/page";
 
 export default function Home() {
   return (
-    <div className="w-full  flex justify-between">
+    <div className="w-full">
       <Sidebar />
       <ProductPage />
     </div>
